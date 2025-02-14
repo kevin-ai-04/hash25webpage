b79f58165ac8a6f17df577a2f3a1803c84984a0a
kevin-ai-04 <117845901+kevin-ai-04@users.noreply.github.com>
2025-02-14 22:05:27 +0530

    Structure Formed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='Container'>
+        <div className='Section 1'>
+          <h1>Section 1</h1>
+        </div>
+        <div className='Section 1'>
+          <h1>Section 2</h1>
+        </div>
+        <div className='Section 1'>
+          <h1>Section 3</h1>
+        </div>
+        <div className='Section 1'>
+          <h1>Section 4</h1>
+        </div>
+        <div className='Section 1'>
+          <h1>Section 5</h1>
+        </div>
+        <div className='Section 1'>
+          <h1>Section 6</h1>
+        </div>
+       </div>
+
     </>
   )
 }
