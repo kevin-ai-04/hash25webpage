c7af7a75696200aff9f05233bcb8074fb5e48c6b
Adhithya Smitha Raj <adhithyasraj7@gmail.com>
2025-02-15 15:20:17 +0530

    updated loading sccreen

--- src/App.jsx
+++ src/App.jsx
@@ -2,65 +2,65 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 import CustomCursor from "./components/CustomCursor";
-import Section1 from './components/section1'
+import Section1 from './components/section1';
 import './fonts.css'; 
 import HappeningSection from './components/section2';
 import CursorGlow from './components/CursorGlow';
+import Section3 from './components/section3';
+import Section4 from './components/section4';
+import Section5 from './components/section5';
+import Section6 from './components/section6';
 
-
-import Section3 from './components/section3'
-import Section4 from './components/section4'
-import Section5 from './components/section5'
-import Section6 from './components/section6'
 function App() {
     const [showLoader, setShowLoader] = useState(true);
-    const [showSection1, setShowSection1] = useState(false);
     const [isFirstLoad, setIsFirstLoad] = useState(true);
-  
+
     useEffect(() => {
-      // Check session storage for first load
-      const firstLoad = sessionStorage.getItem('firstLoad');
-      if (!firstLoad) {
-        sessionStorage.setItem('firstLoad', 'true');
-        setIsFirstLoad(true);
-      } else {
-        setIsFirstLoad(false);
-      }
-  
-      // Loader timeout
-      const timer = setTimeout(() => {
-        setShowLoader(false);
-        setShowSection1(true);
-      }, 5000);
-  
-      return () => clearTimeout(timer);
+        // Check session storage for first load
+        const firstLoad = sessionStorage.getItem('firstLoad');
+        if (!firstLoad) {
+            sessionStorage.setItem('firstLoad', 'true');
+            setIsFirstLoad(true);
+        } else {
+            setIsFirstLoad(false);
+        }
+
+        // Loader timeout (3 seconds)
+        const timer = setTimeout(() => {
+            setShowLoader(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
     }, []);
 
-  return (
-    <>
-      <CursorGlow />
-      <CustomCursor />
-      <div className='Container'>
-      {showLoader && <Section3 autoRotate={true} />}
-        <div className='Section 1'>
-          {showSection1 && <Section1 isFirstLoad={isFirstLoad} />}
-        </div>
-        <div className='Section 2'>
-          <HappeningSection />
-        </div>
-        <div className='Section 4'>
-          <Section4/>
-        </div>
-        <div className='Section 5'>
-          <Section5/>
-        </div>
-        <div className='Section 6'>
-          <Section6/>
-        </div>
-       </div>
-
-    </>
-  );
+    return (
+        <>
+            <CursorGlow />
+            <CustomCursor />
+            
+            {showLoader ? (
+                <Section3 autoRotate={true} />
+            ) : (
+                <div className='Container'>
+                    <div className='Section 1'>
+                        <Section1 isFirstLoad={isFirstLoad} />
+                    </div>
+                    <div className='Section 2'>
+                        <HappeningSection />
+                    </div>
+                    <div className='Section 4'>
+                        <Section4/>
+                    </div>
+                    <div className='Section 5'>
+                        <Section5/>
+                    </div>
+                    <div className='Section 6'>
+                        <Section6/>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
